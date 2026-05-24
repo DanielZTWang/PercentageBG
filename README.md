0cc2718496a93817df81375a9e266920fd5f64ef
Daniel Wang <PakourManiac2011@gmail.com>
2026-05-23 20:23:56 -0400

    Functionality change
    BG to reader

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PercentageBG
-This mod is just for programs to read the percentage more accurately, it is not supposed to be a good mod.
+This mod is just for programs to read percentage/status data, it is not supposed to be a good mod.
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 
+#include <fstream>
+#include <filesystem>
+#include <string>
+
 using namespace geode::prelude;
+
+static std::filesystem::path getOutputPath() {
+    return Mod::get()->getSaveDir() / "gd_percent.txt";
+}
+
+static void writeState(float percent, bool dead, bool completed) {
+    std::ofstream file(getOutputPath(), std::ios::trunc);
+
+    if (!file.is_open()) {
+        return;
+    }
+
+    file << percent << ","
+         << (dead ? "dead" : "alive") << ","
+         << (completed ? "complete" : "incomplete");
+}
 
 class $modify(MyPlayLayer, PlayLayer) {
 
